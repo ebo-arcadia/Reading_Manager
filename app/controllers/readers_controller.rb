39891629e1169dc3faf511f492840285cb295dd6
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-12-13 17:32:11 -0800

    added reader sign up: reader controller.readers new & show pages. url

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,2 +1,28 @@
 class ReadersController < ApplicationController
+
+    def new
+        @reader = Reader.new
+    end 
+
+    def create
+        @reader = Reader.new(reader_params)
+        if @reader.save
+            session[:reader_id] = @reader.id
+            redirect_to reader_path(@reader)
+        else
+            render 'readers/new'
+        end 
+    end 
+
+    def show
+        @reader = Reader.find_by(id: session[:reader_id])
+        redirect_to '/' if !@reader
+    end 
+
+    private
+    
+    def reader_params
+        params.require(:reader).permit(:username, :email, :password)
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   root 'sessions/welcome'
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get 'signup', to: 'reader#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   
   resources :genres
   resources :lists
   resources :books
   resources :readers
+  
 end
