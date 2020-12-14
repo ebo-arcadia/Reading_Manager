7f4db298cfbec50b7c4b524c8456add9f0bf63a4
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-12-13 16:51:20 -0800

    added data validations to Reader

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,4 +1,15 @@
 class Reader < ApplicationRecord
     has_many :books
     has_many :lists, through: :books
+
+    # validates :username, :email, :password, presence: true
+
+    validates :username, presence: { message: 'Please enter a valid username' }
+    validates :email, presence: { message: 'Please enter a valid email' }
+    validates :password, presence: { message: 'Please enter a valid password' }
+
+    validates :username, uniqueness: { message: 'This username is taken, please enter another one'}
+    validates :email, uniqueness: { message: 'This email is taken, please enter another one'}
+    validates :password, length: { in: 8..20 }
+
 end
