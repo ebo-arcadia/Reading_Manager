4daaf66fe4b13aa3966434e670ac92f17c9b881d
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-12-14 09:57:13 -0800

    added nav bar to include login & sign up & logic in view for sign out if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_reader
+    helper_method :current_reader, :logged_in?
 
     def current_reader
         @current_reader ||= Reader.find(session[:reader_id]) if session[:reader_id]
     end 
+
+    def logged_in?
+        !!session[:reader_id]
+    end
+
+    # def redirect_if_not_logged_in
+    #     redirect_to '/ if !logged_in?
+    # end 
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,18 +10,19 @@ class SessionsController < ApplicationController
 
     def create
         # raise params.inspect
-        @reader = Reader.find_by(email: params[:email])
-        if @reader && @reader.authenticate(params[:password])
+        @reader = Reader.find_by(email: params[:reader][:email])
+        if @reader && @reader.authenticate(params[:reader][:password])
             session[:reader_id] = @reader.id
             redirect_to reader_path(@reader)
         else 
+            # flash[:message] = "login credential is incorrect"
             render :new
         end 
     end 
 
     def destroy
         session.delete :reader_id
-        redirect_to '/'
+        redirect_to root_path
     end 
 
 end
