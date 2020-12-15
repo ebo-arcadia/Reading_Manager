ced9c66cf0bba7756643d81cc8f9aafb81f5ea40
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-12-15 14:50:36 -0800

    added create new book on list show page. wired book controller to process params

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,16 @@
 class Book < ApplicationRecord
     belongs_to :reader
     belongs_to :list
+
+    def create
+        @list = List.find(params[:list_id])
+        @book = @list.books.create(book_params)
+        redirect_to list_path(@list)
+    end 
+
+    private 
+        def book_params
+            params.require(:book).permit(:title, :author, :note)
+        end 
+        
 end
