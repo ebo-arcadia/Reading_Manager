5b064bbb959d382020ea124de28bb481cb8d56ae
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-12-20 19:34:38 -0800

    added scope method, custom valdations in list.rb

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,7 +7,9 @@ class List < ApplicationRecord
     # has_many :genres, through: :list_genres
 
     validates :title, :description, presence: true
-    
+    validates :description, length: { maximum: 10, too_long: "%{count} characters is the maximum allowed" }
+    # custom validation
+    validate :daily_create_list_limit
     # accepts_nested_attributes_for :books
 
     delegate :name, to: :genre
@@ -23,6 +25,15 @@ class List < ApplicationRecord
 
     def genre_attributes=(genre)
         self.genre = Genre.find_or_create_by(name: genre[:name]) if !genre[:name].blank?
+    end 
+
+    def daily_create_list_limit
+        today_lists = reader.lists.select do |list|
+            reader.created_at.to_date == Date.today
+        end 
+        if today_lists.size > 3
+            errors.add(:reader_id, "can't created more than 3 reading lists per user a day")
+        end
     end 
     
 end
